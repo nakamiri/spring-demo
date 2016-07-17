c4cc18df962912eac6d77459ec952ea08fdb369a
nakamiri <nakamiri@gmail.com>
2016-07-17 23:49:26 +0900

    Initial commit
    Initial
    
    .gitignore is now working

---
+++ src/main/java/com/example/HelloController.java
@@ -0,0 +1,14 @@
+package com.example;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello-world")
+public class HelloController {
+    @RequestMapping(method=RequestMethod.GET)
+    public String hello() {
+        return "Hello World: Spring MVC";
+    }
+}
